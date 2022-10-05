277d60a926962ae9be72e19b18a6b35350f1ea40
mnia <marcos.ferreira@gft.com>
2022-10-05 15:52:09 -0300

    update links

--- src/app/app-rounting.module.ts
+++ src/app/app-rounting.module.ts
@@ -1,6 +1,4 @@
-import { AdminUpdatePostComponent } from './admin/admin-update-post/admin-update-post.component';
 import { HomeComponent } from './home/home.component';
-import { AdminPostsComponent } from './admin/admin-posts/admin-posts.component';
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
@@ -8,14 +6,6 @@ const routes: Routes = [
   { path: '',
     component: HomeComponent,
     pathMatch: 'full'
-  },
-  {
-    path: 'admin-posts',
-    component: AdminPostsComponent
-  },
-  {
-    path: 'admin-update/:id',
-    component: AdminUpdatePostComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { AppRoutingModule } from './app-rounting.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AdminPostsComponent } from './admin/admin-posts/admin-posts.component';
 import { HttpClientModule} from '@angular/common/http';
-import { HomeComponent } from './home/home.component';
-import { AdminUpdatePostComponent } from './admin/admin-update-post/admin-update-post.component';
-import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    AdminPostsComponent,
     HomeComponent,
-    AdminUpdatePostComponent
   ],
   imports: [
     BrowserModule,
