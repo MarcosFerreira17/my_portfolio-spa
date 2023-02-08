b1a1fe1fbc7c9f3503b0b34f59565641abd11137
mnia <marcos.ferreira@gft.com>
2023-02-08 17:13:17 -0300

    add translation files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,36 @@ import { AppRoutingModule } from './app-rounting.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
     NgxSpinnerModule,
     BrowserAnimationsModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en-US',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,10 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private spinner: NgxSpinnerService) {}
+  constructor(
+    private spinner: NgxSpinnerService,
+    private translateService: TranslateService
+  ) {}
 
   ngOnInit(): void {
     /** spinner starts on init */
@@ -17,5 +21,9 @@ export class HomeComponent implements OnInit {
       /** spinner ends after 1 seconds */
       this.spinner.hide();
     }, 1000);
+  }
+
+  public selectLanguage(event: any) {
+    this.translateService.use(event.target.value);
   }
 }
